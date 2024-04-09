8f7b75bf01280776ce7d454e6e750207fe003341
aarneharju <35223549+aarneharju@users.noreply.github.com>
2024-04-09 20:26:18 +0300

    Keyboard avoiding view functional

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, KeyboardAvoidingView, SafeAreaView, Platform } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, SafeAreaView, Platform, TextInput } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+        <Text>Howdy, enter something something.</Text>
+        <TextInput style={styles.input} value={inputValue} onChangeText={setInputValue} placeholder='Enter text'></TextInput>
+      </KeyboardAvoidingView>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +23,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderBottomColor: "pink",
+    borderBottomWidth: 4,
+    alignSelf: "stretch",
+    margin: 16,
+    padding: 8,
+    backgroundColor: "lightblue"
+  }
 });
